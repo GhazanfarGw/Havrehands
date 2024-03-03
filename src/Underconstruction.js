4e18544b02b62a1268eea751f40bc3b7ea7cdb0a
Muhammad Ghazanfar <116085344+blackhorsese@users.noreply.github.com>
2024-03-03 16:20:48 +0500

    Update Underconstruction.js

--- src/Underconstruction.js
+++ src/Underconstruction.js
@@ -6,7 +6,7 @@ export default function Under() {
     <>
     <div className='max-h-screen mx-auto overflow-hidden bg-[#09121C]'>
       <div className='min-h-screen pt-5'>
-        <img className='text-justify object-cover lg:max-w-4xl justify-center mx-auto px-5' src='./under-construction.png' alt='images'/>
+        <img className='text-justify object-cover lg:max-w-4xl md:max-w-3xl justify-center mx-auto px-5' src='./under-construction.png' alt='images'/>
         <div className=''>
           <Countdown/>
         </div>
